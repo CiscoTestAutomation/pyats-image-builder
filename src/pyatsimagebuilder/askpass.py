07b9392f3dc45a5c7facddc1afbd98a36bc2a5fc
Andrew Delph <adelph@cisco.com>
2020-11-25 17:30:31 -0500

    update to user main function

--- src/pyatsimagebuilder/askpass.py
+++ src/pyatsimagebuilder/askpass.py
@@ -8,12 +8,16 @@
 from sys import argv
 from os import environ
 
-if 'username' in argv[1].lower():
-    print(environ['GIT_USERNAME'])
-    exit()
+def main():
+    if 'username' in argv[1].lower():
+        print(environ['GIT_USERNAME'])
+        exit()
 
-if 'password' in argv[1].lower():
-    print(environ['GIT_PASSWORD'])
-    exit()
+    if 'password' in argv[1].lower():
+        print(environ['GIT_PASSWORD'])
+        exit()
 
-exit(1)
+    exit(1)
+
+if __name__ == '__main__':
+    main()
