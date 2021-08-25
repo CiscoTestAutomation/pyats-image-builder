da2f37e0b100f7a24f2c3138dff13577b372d343
Ben Astell <bastell@cisco.com>
2021-08-25 09:50:05 -0400

    v21.7 bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # launch setup
 setup(
     name = 'pyats-image-builder',
-    version = '21.5.2',
+    version = '21.7',
 
     # descriptions
     description = 'pyATS Docker image creation',
