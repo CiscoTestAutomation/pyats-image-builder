6ae62ad3d05cacfff7ef6f3f4058ae78a6d8cdc2
domachad <domachad@cisco.com>
2022-06-28 13:19:16 -0400

    Release 22.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 # launch setup
 setup(
     name = 'pyats-image-builder',
-    version = '22.5',
+    version = '22.6',
 
     # descriptions
     description = 'pyATS Docker image creation',
